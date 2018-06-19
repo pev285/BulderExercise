ecb8a29a32d8c9207dd7c5c5f5d61b284f19f5c0
MSI-PEV-GP72M\pev <prosolupov@inbox.ru>
2018-06-19 23:54:43 +0300

    Move acceleration.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -9,80 +9,125 @@ public class InputController : MonoBehaviour {
     [SerializeField]
     private Transform myCamera;
     [SerializeField]
-    private float cameraRotationSpeed = 1;
-    private float cameraCurrentAngle = 0;
+    private float verticalRotationSpeed = 1;
+    private float currentCameraAngle = 0;
+    [SerializeField]
     private float maxCameraAngle = 80;
+    [SerializeField]
     private float minCameraAngle = -80;
 
+    [Space(20)]
 
     [SerializeField]
-    private float rotationSpeed = 2f;
-    [SerializeField]
-    private float moveMinSpeed = 1f;
-    [SerializeField]
-    private float moveMaxSpeed = 5f;
-
-
-    [ReadOnly]
-    [SerializeField]
-    private Vector3 currentMoveAmount = Vector3.zero;
-    //private float currentForwardSpeed = 0f;
-    //private float currentSidewardSpeed = 0f;
-    private readonly float sqrt2 = 1.0f / Mathf.Sqrt(2);
-
-    [ReadOnly]
-    [SerializeField]
+    private float horizontalRotationSpeed = 2f;
     private float currentRotationSpeed = 0f;
+
+
+    [Space(20)]
+    [SerializeField]
+    private float minMoveSpeed = 1f;
+    [SerializeField]
+    private float maxMoveSpeed = 5f;
+    [SerializeField]
+    private float moveAcceleration = 1f;
+    [ReadOnly]
+    [SerializeField]
+    private float currentMoveSpeed = 0f;
+
+    [ReadOnly]
+    [SerializeField]
+    private Vector3 currentMoveDirection = Vector3.zero;
+    private readonly float sqrt2 = 1.0f / Mathf.Sqrt(2); // Use coeff if moving along two axis.
 
 
     private Rigidbody rb;
 
-    // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         float horizontalMouseAxis = Input.GetAxis("Mouse X");
         float verticlMouseAxis = Input.GetAxis("Mouse Y");
         float horizontalAxis = Input.GetAxis("Horizontal");
         float verticalAxis = Input.GetAxis("Vertical");
 
+        CalculateHorizontalRotation(horizontalMouseAxis);
 
-        currentRotationSpeed = horizontalMouseAxis * rotationSpeed / Time.deltaTime;
+        CalculateMovement(horizontalAxis, verticalAxis);
 
+        CalculateCameraVerticalRotation(verticlMouseAxis);
 
-        currentMoveAmount = verticalAxis * transform.forward + horizontalAxis * transform.right;
-        if (verticalAxis != 0 && horizontalAxis != 0)
+    }
+
+    private void CalculateCameraVerticalRotation(float verticlMouseAxis)
+    {
+        currentCameraAngle -= verticlMouseAxis * verticalRotationSpeed;
+        currentCameraAngle = Mathf.Clamp(currentCameraAngle, minCameraAngle, maxCameraAngle);
+    }
+
+    private void CalculateMovement(float horizontalAxis, float verticalAxis)
+    {
+        if (verticalAxis != 0 || horizontalAxis != 0)
         {
-            currentMoveAmount *= sqrt2;
+            currentMoveDirection = verticalAxis * transform.forward + horizontalAxis * transform.right;
+            currentMoveDirection.Normalize();
+/*            if (verticalAxis != 0 && horizontalAxis != 0)
+            {
+                currentMoveDirection *= sqrt2;
+            }
+            */
+
+            if (currentMoveSpeed == 0)
+            {
+                currentMoveSpeed = minMoveSpeed;
+            }
+            else
+            {
+                currentMoveSpeed += moveAcceleration * Time.deltaTime;
+            }
+            if (currentMoveSpeed > maxMoveSpeed)
+            {
+                currentMoveSpeed = maxMoveSpeed;
+            }
         }
+        else
+        {
+            currentMoveSpeed -= moveAcceleration * Time.deltaTime;
+            if (currentMoveSpeed < minMoveSpeed)
+            {
+                currentMoveSpeed = 0;
+            }
+        }
+    }
 
-
-        currentMoveAmount *= moveMinSpeed;
-
-
-
-        cameraCurrentAngle -= verticlMouseAxis * cameraRotationSpeed;
-        cameraCurrentAngle = Mathf.Clamp(cameraCurrentAngle, minCameraAngle, maxCameraAngle);
-
-	}
+    private void CalculateHorizontalRotation(float horizontalMouseAxis)
+    {
+        currentRotationSpeed = horizontalMouseAxis * horizontalRotationSpeed / Time.deltaTime;
+    }
 
     private void FixedUpdate()
     {
-        rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(currentRotationSpeed * Time.deltaTime, transform.up));
+        if (currentRotationSpeed != 0)
+        {
+            rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(currentRotationSpeed * Time.deltaTime, transform.up));
+        }
 
-        rb.position += currentMoveAmount * Time.deltaTime;
-
-    }
+        if (currentMoveSpeed != 0)
+        {
+            rb.MovePosition(rb.position + currentMoveDirection * currentMoveSpeed * Time.deltaTime);
+        }
+    } // FixedUpdate() ////
 
     private void LateUpdate()
     {
-        myCamera.localRotation = Quaternion.Euler(cameraCurrentAngle, 0, 0);
-        
+        myCamera.localRotation = Quaternion.Euler(currentCameraAngle, 0, 0);
     }
-}
+
+
+
+} // End of class ///
 
 
 
